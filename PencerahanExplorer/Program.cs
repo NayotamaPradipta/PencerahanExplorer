efc73554e987c55dc02c86e82e9d1ecfb2c82600
chryes220 <christinehutabarat12@gmail.com>
2022-03-14 21:52:47 +0700

    Tambahin BFS

--- PencerahanExplorer/Program.cs
+++ PencerahanExplorer/Program.cs
@@ -3,9 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using PencerahanExplorer;
 
 namespace PencerahanExplorer
 {
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Form2 f1;
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                string path = folderBrowserDialog.SelectedPath;
+                f1 = new Form2(path);
+                Application.Run(f1);
+                string target_name = f1.target_name;
+                Console.WriteLine("Starting folder : " + path);
+                Search.BFS bfs = new Search.BFS(path, target_name);
+                if (bfs.isFound())
+                {
+                    Console.WriteLine("File found in path : " + bfs.target_path);
+                    Form3 f2 = new Form3(path, bfs.target_path);
+                    Application.Run(f2);
+                }
+                else
+                {
+                    Console.WriteLine("File not found ");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Wrong path!");
+            }
+            
+        }
+    }
+    /*
     namespace WindowsFormsApp1
     {
         class ViewerSample // Contoh Visualisasi Graf
@@ -40,6 +81,87 @@ namespace PencerahanExplorer
                 form.ResumeLayout();
                 //show the form
                 form.ShowDialog();
+            }
+        }
+    }
+    */
+    namespace Search
+    { 
+        public class BFS
+        {
+            // belum handle untuk banyak instance dengan nama sama di folder yang berbeda
+            // belum bikin tree
+
+            private bool found;
+            private string target_name;
+            public string target_path; // sementara public
+            private Queue<string> queue;
+
+            public BFS(string start_path, string target_name)
+            {
+                // inisialisasi nilai-nilai atribut
+                found = false;
+                this.target_name = target_name;
+                target_path = "NULL";
+                queue = new Queue<string>();
+                queue.Enqueue(start_path);
+                while (!found && queue.Count > 0)
+                {
+                    string head = queue.Dequeue();
+                    SearchBFS(head);
+                }
+
+            }
+
+            private void SearchBFS(string path)
+            {
+                string[] files = null;
+                try
+                {
+                    // pake exception handling karena kadang nemu restricted folder
+                    files = Directory.GetFiles(path);
+                
+                }
+                catch { }
+                finally
+                {
+                    if (files != null)
+                    {
+                        int i = 0;
+                        // iterate over files
+                        while (!found && i < files.Length)
+                        {
+                            if (Path.GetFileName(files[i]) == target_name)
+                            {
+                                found = true;
+                                target_path = files[i];
+                            }
+                            else
+                            {
+                                i++;
+                            }
+                        }
+                        // file not found in current path, enqueue subdirectories
+                        if (!found)
+                        {
+                            string[] folders = Directory.GetDirectories(path);
+                            for (int k = 0; k < folders.Length; k++)
+                            {
+                                queue.Enqueue(folders[k]);
+                            }
+                        }
+                    }
+                }
+            }
+
+            public bool isFound()
+            {
+                return found;
+            }
+
+            public string get_target_path()
+            {
+                return target_path;
             }
         }
     }
